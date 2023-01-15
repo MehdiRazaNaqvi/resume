79bcf5c4773e1bc1664e51056e7df6567d02ff3e
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2023-01-15 20:21:58 +0500

    initial

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity , Image } from 'react-native';
 
 export default function App() {
+
+
+
+
+
+
+
   return (
+
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text style={{ fontSize: 25, color: "white" }} >SYED MEHDI
+      </Text>
+      {/* 
+      <TouchableOpacity style={styles.color} onPress={() => onPress()}>
+        <Text style={{fontSize:30}} >Profile</Text>
+      </TouchableOpacity> */}
+
     </View>
+
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 0.15,
+    backgroundColor: 'darkgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
+
+
+
+  text: {
+    color: "white",
+    fontWeight: "600"
+  },
+
+  color: {
+    color: "white",
+    backgroundColor: 'red'
+
+  }
+
 });
